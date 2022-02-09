fcdfefd79a77eb09a1016fbcab89ce75fc939fe8
hgu5141 <hgu5141@naver.com>
2022-02-10 01:07:06 +0900

    기능구현 완료

--- src/main/java/com/example/springcore_homework/dto/FoodRequestDto.java
+++ src/main/java/com/example/springcore_homework/dto/FoodRequestDto.java
@@ -1,9 +1,13 @@
 package com.example.springcore_homework.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-public class FoodRequestDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodsRequestDto {
     private String name;
     private int price;
 }
